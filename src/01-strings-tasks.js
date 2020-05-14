6ae9da0e43adc0cb37f9ef034bceefed44124958
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-05-14 16:01:50 +0300

    feat: add solution task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -253,8 +253,10 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string'
+    ? !!value
+    : value instanceof String;
 }
 
 
@@ -282,8 +284,21 @@ function isString(/* value */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  let i = 0;
+
+  const z = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+
+  z.forEach((el, id) => {
+    if (el === value) {
+      i = id;
+    }
+  });
+
+  return i;
 }
 
 
